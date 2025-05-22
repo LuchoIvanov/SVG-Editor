ce5ecea5d6c2c4547da761820a8e189692a72909
lachezar <lachezar.ivanov04@gmail.com>
2025-05-22 13:18:34 +0300

    Updated the translate method to translate all shapes if no index is given

--- src/Commands/TranslateCommand.java
+++ src/Commands/TranslateCommand.java
@@ -1,6 +1,7 @@
 package Commands;
 
 import Application.*;
+import Shapes.Shape;
 
 public class TranslateCommand implements Command{
     /**
@@ -12,13 +13,27 @@ public class TranslateCommand implements Command{
     public void execute(String[] args, FileContext context) {
         int dx = 0;
         int dy = 0;
+        int index = -1;
 
-        for (String part : args) {
-            if (part.startsWith("horizontal=")) dx = Integer.parseInt(part.split("=")[1]);
-            if (part.startsWith("vertical=")) dy = Integer.parseInt(part.split("=")[1]);
+        for (int i = 1; i < args.length; i++) {
+            String part = args[i];
+            if (part.startsWith("horizontal=")) {
+                dx = Integer.parseInt(part.split("=")[1]);
+            } else if (part.startsWith("vertical=")) {
+                dy = Integer.parseInt(part.split("=")[1]);
+            } else index = Integer.parseInt(part);
         }
 
-        context.shapes.get(Integer.parseInt(args[1]) - 1).translate(dx, dy);
-        System.out.println("Shape translated successfully");
+        if (index == -1) {
+            for (Shape shape : context.shapes) {
+                shape.translate(dx, dy);
+            }
+            System.out.println("Translated all figures");
+        } else if (index >= 1 && index <= context.shapes.size()) {
+            context.shapes.get(index - 1).translate(dx, dy);
+            System.out.println("Translated shape " + index);
+        } else {
+            System.out.println("Invalid shape index: " + index);
+        }
     }
 }
